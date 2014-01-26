ea4a9e164d42e169b13be1400a4d00dfb2adc5cd
b-d <bendoyle@umich.edu>
2014-01-25 21:22:34 -0800

    message

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,10 @@
 
-$('body').append('<div class="chrome_pin_ext_panel">Pinterest!!!</div>').append('<a href="javascript:void(0);" class="chrome_pin_ext_slider show"><img src="http://passets-ak.pinterest.com/webapp/style/app/desktop/images/logo_trans_144x144.4d67817d.png" height="20px" width="20px" /></a>');
+$('body').append('<div id="chrome_pin_ext_panel">Pinterest!!!</div>').append('<a href="javascript:void(0);" class="chrome_pin_ext_slider show"><img src="http://passets-ak.pinterest.com/webapp/style/app/desktop/images/logo_trans_144x144.4d67817d.png" height="20px" width="20px" /></a>');
 
 $(function(){
 	$('.chrome_pin_ext_slider').hover(function(){
         if($(this).hasClass('show')){
-	    $( ".chrome_pin_ext_slider, .chrome_pin_ext_panel" ).animate({
+	    $( ".chrome_pin_ext_slider, #chrome_pin_ext_panel" ).animate({
           right: "+=120"
 		  }, 700, function() {
             // Animation complete.
@@ -14,7 +14,7 @@ $(function(){
     });
     $('.chrome_pin_ext_slider').click(function(){
         if($(this).hasClass('hide')){	
-	    $( ".chrome_pin_ext_slider, .chrome_pin_ext_panel" ).animate({
+	    $( ".chrome_pin_ext_slider, #chrome_pin_ext_panel" ).animate({
           right: "-=120"
 		  }, 700, function() {
             // Animation complete.
@@ -24,3 +24,11 @@ $(function(){
     });
 
 });
+
+var theurl =  "https://api.pinterest.com/v3/search/pins/?join=via_pinner,board,pinner&page_size=10&query=Red&access_token=MTQzNTc3NDo0NTA1NzEyNzUxMjgwMTkxMjE6MnwxMzkwNjgwMDYxOjAtLWQ3YzI5NzNiMDJlMGU1YzYxZmY5NjMxNWFmOGZhOGNkY2E2NDU3ZmU=";
+
+$.get( theurl, function( data ) {
+      $( "#chrome_pin_ext_panel" )
+      .append( "Status: " + data.status )
+      .append( "Data: " + data.data );
+      }, "json" );

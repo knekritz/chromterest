124f52105693531486d2f0da4edffe74fd735d57
Kyle Nekritz <knekritz@gmail.com>
2014-01-26 00:26:30 -0500

    on top of everyone else

--- contentscript.js
+++ contentscript.js
@@ -1,25 +1,25 @@
 
-$('body').append('<div id="chrome_pin_ext_panel">Pinterest!!!</div>').append('<a href="javascript:void(0);" class="chrome_pin_ext_slider show"><img src="http://passets-ak.pinterest.com/webapp/style/app/desktop/images/logo_trans_144x144.4d67817d.png" height="20px" width="20px" /></a>');
+$('body').append('<div id="chrome_pin_ext_panel">Pinterest!!!</div>').append('<a href="javascript:void(0);" class="chrome_pin_ext_slider chrome_pin_ext_show"><img src="http://passets-ak.pinterest.com/webapp/style/app/desktop/images/logo_trans_144x144.4d67817d.png" height="20px" width="20px" /></a>');
 
 $(function(){
 	$('.chrome_pin_ext_slider').hover(function(){
-        if($(this).hasClass('show')){
+        if($(this).hasClass('chrome_pin_ext_show')){
 	    $( ".chrome_pin_ext_slider, #chrome_pin_ext_panel" ).animate({
           right: "+=120"
 		  }, 700, function() {
             // Animation complete.
           });
-		  $(this).removeClass('show').addClass('hide');
+		  $(this).removeClass('chrome_pin_ext_show').addClass('chrome_pin_ext_hide');
         }
     });
     $('.chrome_pin_ext_slider').click(function(){
-        if($(this).hasClass('hide')){	
+        if($(this).hasClass('chrome_pin_ext_hide')){	
 	    $( ".chrome_pin_ext_slider, #chrome_pin_ext_panel" ).animate({
           right: "-=120"
 		  }, 700, function() {
             // Animation complete.
           });
-		  $(this).removeClass('hide').addClass('show');    
+		  $(this).removeClass('chrome_pin_ext_hide').addClass('chrome_pin_ext_show');    
         }
     });
 

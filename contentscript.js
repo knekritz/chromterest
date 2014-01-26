5a3653a242c1370909f83a3216577ee0197d048a
b-d <bendoyle@umich.edu>
2014-01-26 02:26:36 -0800

    YO WHATTUP

--- contentscript.js
+++ contentscript.js
@@ -40,41 +40,68 @@ $(function(){
 function func(){
     var query;
     var found = 0;
-    if ($('input[name=q]').length) {
+    if ($('input[name=q]').length && $('input[name=q]').val().length) {
         query = $('input[name=q]').val();
     }
-    else if ($('input[name=field-keywords]').length) {
+    else if ($('input[name=field-keywords]').length && $('input[name=field-keywords]').val().length) {
         query = $('input[name=field-keywords]').val();
     }
-    else if ($('input[name=query]').length) {
+    else if ($('input[name=query]').length && $('input[name=query]').val().length) {
         query = $('input[name=query]').val();
     }
-    else if ($('input[name=term]').length) {
+    else if ($('input[name=term]').length && $('input[name=term]').val().length) {
         query = $('input[name=term]').val();
     }
-    else if ($('input[name=field-keywords]').length) {
-        query = $('input[name=field-keywords]').val();
+    else if ($('h1').length && $('h1').text().length > 9) {
+        query = $('h1').text();
     }
     else {
-        var keywords = glossary.extract("")
-        alert(keywords);
-        query = $('input[name=field-keywords]').val();
+        query = $('title').text();
     }
     
     
     
 
 var theurl =  "https://api.pinterest.com/v3/search/pins/?page_size=50&query=" + query+"&access_token=MTQzNTc3NDo0NTA1NzEyNzUxMjgwMTkxMjE6MnwxMzkwNjgwMDYxOjAtLWQ3YzI5NzNiMDJlMGU1YzYxZmY5NjMxNWFmOGZhOGNkY2E2NDU3ZmU=";
-
+    var found = 0;
 
 $.get( theurl, function( data ) {
       $( "#chrome_pin_ext_panel" ).empty();
        for (i=0;i<data.data.length;i++) {
+      found = 1;
             $( "#chrome_pin_ext_panel" )
             .append( "\n")
             .append('<a href="http://www.pinterest.com/pin/'+data.data[i].id+'"><img Title="'+data.data[i].description+'" class="chrome_pin_ext_pinimage" src="' + data.data[i].image_medium_url +'"/></a>');
       }
       }, "json" );
+    
+    if (!found){
+        var foundword = 0;
+        var list = query.split(" ");
+        for (i=0; i<list.length;i++) {
+            if (list[i].length > 3) {
+                foundword = 1;
+                query = list[i];
+                break;
+            }
+            
+        }
+        if (!foundword) {
+            query = "you suck";
+        }
+        
+        theurl =  "https://api.pinterest.com/v3/search/pins/?page_size=50&query=" + query+"&access_token=MTQzNTc3NDo0NTA1NzEyNzUxMjgwMTkxMjE6MnwxMzkwNjgwMDYxOjAtLWQ3YzI5NzNiMDJlMGU1YzYxZmY5NjMxNWFmOGZhOGNkY2E2NDU3ZmU=";
+    $.get( theurl, function( data ) {
+          $( "#chrome_pin_ext_panel" ).empty();
+          for (i=0;i<data.data.length;i++) {
+          $( "#chrome_pin_ext_panel" )
+          .append( "\n")
+          .append('<a href="http://www.pinterest.com/pin/'+data.data[i].id+'"><img Title="'+data.data[i].description+'" class="chrome_pin_ext_pinimage" src="' + data.data[i].image_medium_url +'"/></a>');
+          }
+          }, "json" );
+    }
+    
+    
 }
 func();
 $(window).on('change',function(){
